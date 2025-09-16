4d83cdb7227e630d024b517482651138757257d5
Chanuka  Induwara <IT24104358@my.sliit.lk>
2025-09-16 23:07:36 +0530

    IT24104358_Lab07

---
+++ Lab Sheet 07/IT24104358.R
@@ -0,0 +1,5 @@
+setwd("C:\\Users\\lap.lk\\Desktop\\IT24104358")
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+pexp(2, rate = 1 / 3, lower.tail = TRUE)
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
